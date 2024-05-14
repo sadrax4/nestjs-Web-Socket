d4e2e45bd245f324d471dc6ac4b8d0ce6c02231d
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-05-14 23:15:20 +0330

    refactor: docker-file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MessageModule } from './message/message.module';
 
 @Module({
   imports: [MessageModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
