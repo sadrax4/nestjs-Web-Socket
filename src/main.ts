ab3392b2af865e5549ee101dca0fe5915da72cd0
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-05-18 22:46:18 +0330

    init: redis in bootstrat

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './redis/redis-io-adaptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
   await app.listen(3000);
 }
 bootstrap();
